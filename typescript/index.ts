fd2e34c729bf72f34ae03dafa9fc638f7f9e3982
Len Rich <lenrich1403@gmail.com>
2024-02-28 11:27:10 +0800

    Abstraction Implementation

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,47 +1,14 @@
-// shapes
-// Area, Perimeter
-// simple - single fucntion caculateTotalArea
+// Date Class
+// Get Current Year
+// Get Current Month
+// Get Current Date
 
-// Interface A Shape
-interface Shape {
-  area(): number;
-  perimeter(): number;
-}
+const now = new Date();
 
-class Circle implements Shape {
-  constructor(private radius: number) {}
+const currentYear = now.getFullYear();
+const currentMonth = now.getMonth() + 1;
+const currentDate = now.getDate();
 
-  area(): number {
-    return Math.PI ** this.radius;
-  }
-
-  perimeter(): number {
-    return 2 * Math.PI ** this.radius;
-  }
-}
-
-class Rectangle implements Shape {
-  constructor(
-    private width: number,
-    private height: number,
-  ) {}
-  area(): number {
-    return this.width * this.height;
-  }
-
-  perimeter(): number {
-    return 2 * (this.width + this.height);
-  }
-}
-
-function calculateTotalArea(shape: Shape): number {
-  return shape.area();
-}
-
-
-// Client Code
-let circle: Circle = new Circle(5);
-let rectangle: Rectangle = new Rectangle(4, 6);
-
-console.log(`Area of Circle: ${calculateTotalArea(circle)}`);
-console.log(`Area of Reactangle: ${calculateTotalArea(rectangle)}`);
+console.log(currentYear);
+console.log(currentMonth);
+console.log(currentDate);
